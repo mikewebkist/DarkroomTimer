300ca63556a0537a0189a220fc89c27d5f6611a1
Michael Cramer <cramer@webkist.com>
2010-01-13 16:42:31 -0500

    filling in the content provider

--- src/com/webkist/android/DarkroomTimer/DarkroomPresetProvider.java
+++ src/com/webkist/android/DarkroomTimer/DarkroomPresetProvider.java
@@ -1,19 +1,25 @@
 package com.webkist.android.DarkroomTimer;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.content.res.Resources;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
+import java.lang.*;
 
 public class DarkroomPresetProvider extends ContentProvider {
 	public static final String AUTHORITY = "com.webkist.android.DarkroomTimer";
 	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+	public static final Uri CONTENT_URI_PRESET = Uri.parse("content://" + AUTHORITY + "/preset");
 	private static final String TAG = "DarkroomPresetProvider";
 	
 	private static final String DATABASE_NAME = "presets.db";
@@ -21,14 +27,17 @@ public class DarkroomPresetProvider extends ContentProvider {
 
 	public static final String PRESET_CONTENT_TYPE = "vnd.android.cursor.item/vnd.webkist.preset";
 	public static final String PRESET_CONTENT_TYPE_LIST = "vnd.android.cursor.dir/vnd.webkist.preset";
-	private static final int PRESETS = 1;
-	private static final int PRESET_ID = 2;
+	private static final int URI_PRESETS = 1;
+	private static final int URI_PRESET_ID = 2;
+	private static final int URI_STEP_ID = 3;
 	private static final UriMatcher sUriMatcher;
 
 	private static final String PRESET_TABLE_NAME = "presets";
 	public static final String PRESET_NAME = "name";
 	
-	private static final String STEPS_TABLE_NAME = "steps";
+	private static final String STEP_TABLE_NAME = "steps";
+	public static final String STEP_PRESET = "preset";
+	public static final String STEP_STEP = "step";
 	public static final String STEP_NAME = "name";
 	public static final String STEP_DURATION = "duration";
 	public static final String STEP_AGITATION = "agitation";
@@ -72,17 +81,28 @@ public class DarkroomPresetProvider extends ContentProvider {
 //	}
 
 	@Override
-	public int delete(Uri arg0, String arg1, String[] arg2) {
-		// TODO Auto-generated method stub
+	public int delete(Uri uri, String where, String[] whereArgs) {
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+		int count;
+		switch(sUriMatcher.match(uri)) {
+		case URI_PRESETS:
+			Log.w(TAG, "Deleting all presets is not allowed!");
+			break;
+		case URI_PRESET_ID:
+			Log.w(TAG, "Deleting preset id=" + uri.getPathSegments().get(1));
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
 		return 0;
 	}
 
 	@Override
 	public String getType(Uri uri) {
 		switch(sUriMatcher.match(uri)) {
-		case PRESETS:
+		case URI_PRESETS:
 			return PRESET_CONTENT_TYPE_LIST;
-		case PRESET_ID:
+		case URI_PRESET_ID:
 			return PRESET_CONTENT_TYPE;
 		default:
 			throw new IllegalArgumentException("Unknown URI " + uri);
@@ -90,16 +110,69 @@ public class DarkroomPresetProvider extends ContentProvider {
 	}
 
 	@Override
-	public Uri insert(Uri arg0, ContentValues arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	public Uri insert(Uri uri, ContentValues initialValues) {
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+		ContentValues values;
+		if(initialValues != null) {
+			values = new ContentValues(initialValues);
+		} else {
+			values = new ContentValues();
+		}
+		
+		long rowId;
+				
+		switch(sUriMatcher.match(uri)) {
+		case URI_PRESET_ID:
+			rowId = db.insert(PRESET_TABLE_NAME, PRESET_NAME, values);
+			if(rowId > 0) {
+				Uri presetUri = ContentUris.withAppendedId(CONTENT_URI_PRESET, rowId);
+				getContext().getContentResolver().notifyChange(presetUri, null);
+				return presetUri;
+			}
+			break;
+		case URI_STEP_ID:
+			// Deal with default values in the DarkroomPreset class.
+			rowId = db.insert(STEP_TABLE_NAME, STEP_NAME, values);
+			if(rowId > 0) {
+				Uri presetUri = ContentUris.withAppendedId(CONTENT_URI_PRESET, Long.decode(values.get(STEP_PRESET).toString()));
+				getContext().getContentResolver().notifyChange(presetUri, null);
+				return presetUri;
+			}
+			break;
+		default:
+			throw new IllegalArgumentException("Can't insert: " + uri);
+		}
+
+		throw new SQLException("Failed to insert row into " + uri);
 	}
 
 	@Override
-	public Cursor query(Uri arg0, String[] arg1, String arg2, String[] arg3,
-			String arg4) {
-		// TODO Auto-generated method stub
-		return null;
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+		
+		String orderBy;
+		
+		switch(sUriMatcher.match(uri)) {
+		case URI_PRESETS: // Get all presets
+			qb.setTables(PRESET_TABLE_NAME);
+			orderBy = PRESET_NAME;
+			break;
+		
+		case URI_PRESET_ID: // Get steps associated with a preset
+			qb.setTables(STEP_TABLE_NAME);
+			qb.appendWhere(STEP_PRESET + "=" + uri.getPathSegments().get(1));
+			orderBy = STEP_STEP;
+			break;
+			
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+		
+		SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+		Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, orderBy);
+		c.setNotificationUri(getContext().getContentResolver(), uri);
+		return c;		
 	}
 
 	@Override
@@ -110,7 +183,8 @@ public class DarkroomPresetProvider extends ContentProvider {
 	
 	static {
 		sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-		sUriMatcher.addURI(AUTHORITY, "presets", PRESETS);
-		sUriMatcher.addURI(AUTHORITY, "preset/#", PRESET_ID);
+		sUriMatcher.addURI(AUTHORITY, "presets", URI_PRESETS);
+		sUriMatcher.addURI(AUTHORITY, "preset/#", URI_PRESET_ID);
+		sUriMatcher.addURI(AUTHORITY, "step/#", URI_STEP_ID);
 	}
 }
